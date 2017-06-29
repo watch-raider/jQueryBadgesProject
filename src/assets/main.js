31f51fde6e2e87aefc3348ea1014f8ea701443c5
watch-raider <07waltonmi@gmail.com>
2017-06-29 15:55:16 +0100

    changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,10 +1,12 @@
 $(function() {
 
   // your code will go here
-  jQuery.ajax({
+  $.ajax({
     url: 'https://www.codeschool.com/users/advancedInnovation.json',
     dataType: 'jsonp',
     success: function(response) {
       // handle response
+      console.log('response');
     }
+  })
 });
